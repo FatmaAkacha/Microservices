5d89d6280201ee2a49442dc9fd0961c448e7fae3
FatmaAkacha <akachafatma1@gmail.com>
2024-11-19 13:53:36 +0100

    Ajoutez des fichiers projet.

---
+++ Orders/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Orders
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ OrdersAPI/Data/OrdersAPIContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace OrdersAPI.Data
+{
+    public class OrdersAPIContext : DbContext
+    {
+        public OrdersAPIContext (DbContextOptions<OrdersAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Model.Orders> Orders { get; set; } = default!;
+        public DbSet<Product> Products { get; set; }
+    }
+}
